aff0d53d2a864669208af9e5aaf5a38203dd4c79
Christelle Cossard <christelle.cossard10@gmail.com>
2023-01-30 08:32:17 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//projet créé le 26 janv 2023
+//projet ECF été 2023 - site d'un restaurant
+
+  require_once('templates/header.php');
+  //echo 'hello world!';
+?>
+
+<?php
+require_once('templates/footer.php');
+?>
---
+++ templates/footer.php
@@ -0,0 +1,44 @@
+<footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+      <div class="col mb-3">
+        <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+          <svg class="bi me-2" width="40" height="32">
+            <use xlink:href="#bootstrap"></use>
+          </svg>
+        </a>
+        <p class="text-muted">© 2022</p>
+      </div>
+      <div class="col mb-3">
+      </div>
+      <div class="col mb-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">About</a></li>
+        </ul>
+      </div>
+      <div class="col mb-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">PriX</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">About</a></li>
+        </ul>
+      </div>
+      <div class="col mb-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">About</a></li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+
