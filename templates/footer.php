4e2d7a1de72fc805b4d290fbc9235e0f8f06c0bb
Christelle Cossard <christelle.cossard10@gmail.com>
2023-01-30 08:42:49 +0100

    premiére modif dans footer

--- templates/footer.php
+++ templates/footer.php
@@ -12,7 +12,7 @@
       <div class="col mb-3">
         <h5>Section</h5>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link">ACCUEIL</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link">Features</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link">Pricing</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link">FAQs</a></li>
