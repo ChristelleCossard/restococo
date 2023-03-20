d91df3c4dad28d7aff39e7d1ecbbd9edf3e5d8ff
Christelle Cossard <christelle.cossard10@gmail.com>
2023-03-20 06:49:52 +0100

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,22 +1,23 @@
 
 <hr>
+<footer>
         <div class="container-fluid">
             <div class="row footer">
                 <div class="d-flex justify-content-center">
                     <div class="col-2">
                         <ul class="list-unstyled text-center">
                            
-                            <li><a class="link-footer" href="mailto:christelle.cossard10@gmail.com<">christelle.cossard10@gmail.com</a></li>
+                            <li><a class="nav-link" href="mailto:christelle.cossard10@gmail.com<">christelle.cossard10@gmail.com</a></li>
                         </ul>
                     </div>
                     <div class="col-6 text-center copyright">
                         <br>
                         © 2023 
-		  <a href="http://cocoweb10.fr/" target="_blank"> Christelle Cossard.</a>
+		  <a class="nav-link" href="http://cocoweb10.fr/" target="_blank"> Christelle Cossard.</a>
                     </div>
                     <div class="col-2">
                         <ul class="list-unstyled text-center">
-                            <li><a class="link-footer" href="legal.php">Mentions légales</a></li>
+                            <li><a class="nav-link" href="legal.php">Mentions légales</a></li>
                         <!--  /*** CI-dessous: liens à modifier ****************/ -->
                           <!--
                             <li><a class="link-footer" href="https://www.linkedin.com/in/kevin-combar/"><i class="bi bi-linkedin"></i></a> <a class="link-footer" href="https://twitter.com/kevincombar"><i class="bi bi-twitter"></i></a> <a class="link-footer" href="https://www.instagram.com/kevincombar/"><i class="bi bi-instagram"></i></a> <a class="link-footer" href="https://github.com/kevincombar"><i class="bi bi-github"></i></a></li>
@@ -27,51 +28,6 @@
             </div>
         </div>
 
-<footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-      
-
-               
-
-<div class="col mb-3">
-        <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-          <svg class="bi me-2" width="40" height="32">
-            <use xlink:href="#bootstrap"></use>
-          </svg>
-        </a>
-       
-      </div>
-      <div class="col mb-3">
-      </div>
-      <div class="col mb-3">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link">ACCUEIL</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">About</a></li>
-        </ul>
-      </div>
-      <div class="col mb-3">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">PriX</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">About</a></li>
-        </ul>
-      </div>
-      <div class="col mb-3">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link">About</a></li>
-        </ul>
-      </div>
-    </footer>
+</footer>
   </div>
 
